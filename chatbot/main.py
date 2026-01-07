db7be91aed8ca2cf4180fa04b7fdddd303f243df
Filipe Barros <filipe.bc@live.com>
2026-01-07 20:45:56 -0300

    chatbot init

---
+++ chatbot/main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello from chatbot!")
+
+
+if __name__ == "__main__":
+    main()
+# Imports
+import streamlit
+import nltk
+from nltk.chat.util import Chat, reflections
+import json
+
+# Carregar dataset público (pairs.json)
